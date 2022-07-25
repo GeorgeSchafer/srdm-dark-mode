b500a1e66ee2827a9f1dbd7f606e713106679196
George <george.lmfao@gmail.com>
2022-07-25 16:28:29 -0700

    ReArranged for clarity.

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,43 @@
 "use strict";
 
+function theThing(tab,url){
+
+  const Elements = { 
+    divs: document.getElementsByTagName("div"), // Retrive an array of divs
+    h1: document.getElementsByTagName("h1"),
+    h2: document.getElementsByTagName("h2"), 
+    h3: document.getElementsByTagName("h3"), 
+    h4: document.getElementsByTagName("h4"), 
+    h5: document.getElementsByTagName("h5"),
+    input: document.getElementsByTagName(`input[type="text"]`)
+  };
+  
+  const Listeners = [ 
+    document.onload(stripInlineStyleColors()) 
+  ];
+  
+  const Functions = {
+    stripInlineStyleColors: () => {
+      const bundle = { 
+        divs: Elements.divs,
+        h1: Elements.h1,
+        h2: Elements.h2,
+        h3: Elements.h3,
+        h4: Elements.h4,
+        h5: Elements.h5,
+        input: Elements.input
+      }
+  
+      for(const [key,value] of Object.entries(bundle) ){
+        console.log(`Stripping ${value}`);
+        value.removeProperty("background-color"); // Remove inline tag
+        value.removeProperty("color");  // Remove text color
+        value.removeProperty("box-shadow");  // Remove box shadow
+      }
+    }
+  };
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   const tab = tabs[0];
   const url = new URL(tab.url)
@@ -16,42 +54,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   /** @summary IFF we are on SupportRent or CMW. 
    * Otherwise, don't do the thing. */
   if( SRDomain ){
-
-    /* for smartrent CMW && SupportRent only */
-    const Elements = { 
-      divs: document.getElementsByTagName("div"), // Retrive an array of divs
-      h1: document.getElementsByTagName("h1"),
-      h2: document.getElementsByTagName("h2"), 
-      h3: document.getElementsByTagName("h3"), 
-      h4: document.getElementsByTagName("h4"), 
-      h5: document.getElementsByTagName("h5"),
-      input: document.getElementsByTagName(`input[type="text"]`)
-    };
-    
-    const Listeners = [ 
-      document.onload(stripInlineStyleColors()) 
-    ];
-    
-    const Functions = {
-      stripInlineStyleColors: () => {
-        const bundle = { 
-          divs: Elements.divs,
-          h1: Elements.h1,
-          h2: Elements.h2,
-          h3: Elements.h3,
-          h4: Elements.h4,
-          h5: Elements.h5,
-          input: Elements.input
-        }
-    
-        for(const [key,value] of Object.entries(bundle) ){
-          console.log(`Stripping ${value}`);
-          value.removeProperty("background-color"); // Remove inline tag
-          value.removeProperty("color");  // Remove text color
-          value.removeProperty("box-shadow");  // Remove box shadow
-        }
-    
-      }
-    };
+    theThing(tab,url);
   }
+
 });
