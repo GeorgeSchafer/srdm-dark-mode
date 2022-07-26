5da54ab477ce7a34e59df3bd8ad93a4192f297f0
George <george.lmfao@gmail.com>
2022-07-25 17:50:21 -0700

    added call for stripInlineStyleColors()

--- scripts/content.js
+++ scripts/content.js
@@ -1,65 +1,55 @@
 "use strict";
 
-function doTheThing(tab,url){
+/**
+ * @const Elements
+ * We are storing the elements that need to be changed.
+ */
+const Elements = { 
+  divs: document.getElementsByTagName("div"),
+  h1: document.getElementsByTagName("h1"),
+  h2: document.getElementsByTagName("h2"), 
+  h3: document.getElementsByTagName("h3"), 
+  h4: document.getElementsByTagName("h4"), 
+  h5: document.getElementsByTagName("h5"),
+  input: document.getElementsByTagName(`input[type="text"]`)
+};
 
-  /**
-   * @const Elements
-   * Here we are finding
-   */
-  const Elements = { 
-    divs: document.getElementsByTagName("div"),
-    h1: document.getElementsByTagName("h1"),
-    h2: document.getElementsByTagName("h2"), 
-    h3: document.getElementsByTagName("h3"), 
-    h4: document.getElementsByTagName("h4"), 
-    h5: document.getElementsByTagName("h5"),
-    input: document.getElementsByTagName(`input[type="text"]`)
-  };
-  
-  const Listeners = [ 
-    document.onload(stripInlineStyleColors()) 
-  ];
-  
-  const Functions = {
-    stripInlineStyleColors: () => {
-      const bundle = { 
-        divs: Elements.divs,
-        h1: Elements.h1,
-        h2: Elements.h2,
-        h3: Elements.h3,
-        h4: Elements.h4,
-        h5: Elements.h5,
-        input: Elements.input
-      }
-  
-      for(const [key,value] of Object.entries(bundle) ){
-        value.forEach( (index) => {
-          index.removeProperty("background-color");
-          index.removeProperty("color");
-          index.removeProperty("box-shadow");
-        } )
-      }
+/**
+  * @const Listeners
+  * We are storing the listeners in arrays because we 
+  * need them to be there but we don't need to retrieve 
+  * them ever.
+*/
+const Listeners = [ 
+  document.onload(stripInlineStyleColors()) 
+];
+
+const Functions = {
+  stripInlineStyleColors: () => {
+    const bundle = { 
+      divs: Elements.divs,
+      h1: Elements.h1,
+      h2: Elements.h2,
+      h3: Elements.h3,
+      h4: Elements.h4,
+      h5: Elements.h5,
+      input: Elements.input
     }
-  };
-}
 
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  const tab = tabs[0];
-  const url = new URL(tab.url)
-  const domain = url.hostname
-  // `domain` now has a value like "example.com"
+    console.log(JSON.parse(JSON.stringify(bundle)));
 
-  const SRDomain = (
-    domain === "support.qa-smartrent.com" || 
-    domain === "support.smartrent.com"|| 
-    domain === "control.smartrent-qa.com" || 
-    domain === "control.smartrent.com"
-  );
-
-  /** @summary IFF we are on SupportRent or CMW. 
-   * Otherwise, don't do the thing. */
-  if( SRDomain ){
-    doTheThing(tab,url);
+    for(const [key,value] of Object.entries(bundle) ){
+      console.log(`${key}`);
+      value.forEach( (index) => {
+        index.style.removeProperty("background-color");
+        index.style.backgroundColor( "" );
+        index.style.removeProperty("color");
+        index.style.color( "" );
+        index.style.removeProperty("box-shadow");
+        index.style.boxShadow( "" );
+      })
+    }
   }
+};
 
-});
+Functions.stripInlineStyleColors();
